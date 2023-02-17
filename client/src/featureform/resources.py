57ae3031d69695d0d42b50c150127eb8a261797b
sdreyer <sterling@featureform.com>
2023-02-17 11:28:00 -0800

    Added exception handling

--- client/src/featureform/resources.py
+++ client/src/featureform/resources.py
@@ -1792,7 +1792,7 @@ class ResourceState:
                     print(f"{resource.name} {resource_variant} already exists.")
                     continue
 
-                raise e
+                raise Exception(f"Code: {e.code()}: {e.details()}")
 
 
 ## Executor Providers
